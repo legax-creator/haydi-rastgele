46084d2ec51584bbf9863032f5f9cd926dede320
legax-creator <my3reeder@gmail.com>
2026-07-12 01:21:59 +0300

    Update Haydirastgele.java

--- src/main/java/com/haydirastgele/Haydirastgele.java
+++ src/main/java/com/haydirastgele/Haydirastgele.java
@@ -1,1 +1,24 @@
 
+package com.haydirastgele;
+
+import com.haydirastgele.events.GameEvents;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("haydirastgele")
+public class Haydirastgele {
+
+    public Haydirastgele() {
+        // Ortak kurulum olayını dinlemeye alıyoruz
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Yazdığımız tüm GameEvents olaylarını Minecraft'ın ana olay yöneticisine kaydediyoruz
+        MinecraftForge.EVENT_BUS.register(GameEvents.class);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // Mod ilk açıldığında yapılması gerekenler buraya eklenebilir
+    }
+}
