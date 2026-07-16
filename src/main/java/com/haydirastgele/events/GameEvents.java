8424a54a08b1924c5c955ea8152f0a827f82d91b
legax-creator <my3reeder@gmail.com>
2026-07-16 18:14:06 +0300

    Update GameEvents.java

--- src/main/java/com/haydirastgele/events/GameEvents.java
+++ src/main/java/com/haydirastgele/events/GameEvents.java
@@ -1,17 +1,12 @@
 package com.haydirastgele.events;
 
 import com.haydirastgele.utils.MobManager;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityDimensions;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -24,13 +19,22 @@ import net.minecraftforge.fml.common.Mod;
 public class GameEvents {
 
     @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END && event.player instanceof ServerPlayer player) {
+            MobManager.tickQuest(player);
+            
+            if (!MobManager.activeQuestType.equals("NONE")) {
+                MobManager.applyFormRestrictions(player);
+            }
+        }
+    }
+
+    @SubscribeEvent
     public static void onPlayerDeath(LivingDeathEvent event) {
         if (event.getEntity() instanceof ServerPlayer player) {
-            // Eğer aktif bir kaos görevi varken öldüyse görevi başarısız say (-10 Karma)
             if (!MobManager.activeQuestType.equals("NONE")) {
                 MobManager.completeQuest(player, false);
             } else {
-                // Normal ölüm cezası (-20 Karma)
                 MobManager.handlePlayerDeath(player);
             }
         }
@@ -44,115 +48,85 @@ public class GameEvents {
     }
 
     @SubscribeEvent
-    public static void onPlayerUpdate(LivingEvent.LivingTickEvent event) {
+    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
         if (event.getEntity() instanceof ServerPlayer player) {
-            String form = MobManager.currentMobForm.toLowerCase();
-            
-            // Koşma engeli kontrolü
-            if (player.isSprinting() && !MobManager.canMobSprint(form)) {
-                player.setSprinting(false);
-            }
-
-            // Görev zamanlayıcı sistemini tetikle
-            MobManager.tickQuest(player);
+            MobManager.assignNewMob(player);
+            MobManager.applyFormSpawnLocation(player);
         }
     }
 
     @SubscribeEvent
+    public static void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
+        MobManager.handleInteract(event);
+    }
+
+    @SubscribeEvent
+    public static void onLivingAttack(LivingAttackEvent event) {
+        MobManager.handleAttack(event);
+    }
+
+    @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
-        Player player = event.getPlayer();
-        if (player instanceof ServerPlayer serverPlayer) {
-            if (MobManager.isInteractionRestricted(serverPlayer)) {
+        if (event.getPlayer() instanceof ServerPlayer player) {
+            if (MobManager.isInteractionRestricted(player)) {
                 event.setCanceled(true);
-                serverPlayer.sendSystemMessage(net.minecraft.network.chat.Component.literal("§c[!] Bu formdayken blok kıramazsınız!"));
             }
         }
     }
 
     @SubscribeEvent
     public static void onBlockPlace(BlockEvent.EntityPlaceEvent event) {
-        if (event.getEntity() instanceof ServerPlayer serverPlayer) {
-            if (MobManager.isInteractionRestricted(serverPlayer)) {
-                event.setCanceled(true);
-                serverPlayer.sendSystemMessage(net.minecraft.network.chat.Component.literal("§c[!] Bu formdayken blok yerleştiremezsiniz!"));
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-        if (event.getEntity() instanceof ServerPlayer serverPlayer) {
-            if (MobManager.isInteractionRestricted(serverPlayer)) {
-                event.setCanceled(true);
-                serverPlayer.sendSystemMessage(net.minecraft.network.chat.Component.literal("§c[!] Bu formdayken bloklarla etkileşime giremezsiniz!"));
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public static void onPlayerInteract(PlayerInteractEvent.EntityInteract event) {
-        MobManager.handleInteract(event);
-    }
-
-    @SubscribeEvent
-    public static void onPlayerAttack(LivingAttackEvent event) {
-        MobManager.handleAttack(event);
-    }
-
-    @SubscribeEvent
-    public static void onRightClickItem(PlayerInteractEvent.RightClickItem event) {
         if (event.getEntity() instanceof ServerPlayer player) {
-            String currentForm = MobManager.currentMobForm.toLowerCase();
-
-            if (event.getItemStack().is(Items.GOAT_HORN) && MobManager.hasSpecialAbility(currentForm)) {
-                MobManager.triggerFormAbility(player);
-                int cooldownTicks = MobManager.getCooldownTicks(currentForm);
-                player.getCooldowns().addCooldown(Items.GOAT_HORN, cooldownTicks);
-            }
-            
-            String foodName = event.getItemStack().getItem().toString();
-            if (event.getItemStack().isEdible() && !MobManager.canEatFood(currentForm, foodName)) {
+            if (MobManager.isInteractionRestricted(player)) {
                 event.setCanceled(true);
-                player.sendSystemMessage(net.minecraft.network.chat.Component.literal("§c[!] Mevcut formunuz bu yiyeceği tüketemez!"));
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerRightClick(PlayerInteractEvent.RightClickItem event) {
+        Player player = event.getEntity();
+        ItemStack stack = event.getItemStack();
+
+        if (!player.level().isClientSide() && player instanceof ServerPlayer serverPlayer) {
+            if (stack.is(Items.GOAT_HORN)) {
+                String form = MobManager.currentMobForm.toLowerCase();
+                if (MobManager.hasSpecialAbility(form)) {
+                    MobManager.triggerFormAbility(serverPlayer);
+                    event.getEntity().swing(event.getHand(), true);
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onFoodRightClick(PlayerInteractEvent.RightClickItem event) {
+        Player player = event.getEntity();
+        ItemStack stack = event.getItemStack();
+
+        if (stack.getItem().isEdible()) {
+            String foodName = stack.getItem().toString();
+            if (!MobManager.canEatFood(MobManager.currentMobForm, foodName)) {
+                event.setCanceled(true);
+                if (!player.level().isClientSide()) {
+                    player.sendSystemMessage(net.minecraft.network.chat.Component.literal(
+                        "§c[!] Mevcut formunuz bu yiyeceği tüketemez!"
+                    ));
+                }
             }
         }
     }
 
     @SubscribeEvent
     public static void onPlayerSize(EntityEvent.Size event) {
-        if (event.getEntity() instanceof Player player) {
-            String form = MobManager.currentMobForm.toLowerCase();
-            if (!form.equals("human")) {
+        if (event.getEntity() instanceof Player) {
+            String form = MobManager.currentMobForm;
+            if (!form.equalsIgnoreCase("human")) {
                 float width = MobManager.getMobWidth(form);
                 float height = MobManager.getMobHeight(form);
                 float eyeHeight = MobManager.getMobEyeHeight(form);
                 
-                event.setNewSize(EntityDimensions.scalable(width, height));
-                event.setNewEyeHeight(eyeHeight);
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public static void onPlayerRender(RenderPlayerEvent.Pre event) {
-        Player player = event.getEntity();
-        String form = MobManager.currentMobForm.toLowerCase();
-
-        if (!form.equals("human")) {
-            event.setCanceled(true);
-            
-            if (player.level().isClientSide) {
-                EntityRenderDispatcher dispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
-                EntityType<?> type = EntityType.byString(form).orElse(EntityType.PIG); 
-                Entity mobEntity = type.create(player.level());
-                
-                if (mobEntity != null) {
-                    mobEntity.setPos(player.getX(), player.getY(), player.getZ());
-                    mobEntity.setYRot(player.getYRot());
-                    mobEntity.setXRot(player.getXRot());
-                    
-                    dispatcher.render(mobEntity, 0, 0, 0, player.getYRot(), event.getPartialTick(), event.getPoseStack(), event.getMultiBufferSource(), event.getPackedLight());
-                }
+                event.setNewSize(net.minecraft.world.entity.EntityDimensions.scalable(width, height), eyeHeight);
             }
         }
     }
